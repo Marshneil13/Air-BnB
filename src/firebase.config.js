8da2d64258547609a4b3e7bdb0e17c89ae7fe7f1
marshnil_13 <btech10225.20@bitmesra.ac.in>
2022-05-12 11:56:02 +0530

    Firebase setup

---
+++ src/firebase.config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFireStore } from "firebase/firestore"
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDh9Qq__CIRizz6TDbZKFqk5Vs9XF4kYkk",
+  authDomain: "air-b2b-app.firebaseapp.com",
+  projectId: "air-b2b-app",
+  storageBucket: "air-b2b-app.appspot.com",
+  messagingSenderId: "579768326228",
+  appId: "1:579768326228:web:2d80ce0d9231de799835c1"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+export const db = getFireStore()
