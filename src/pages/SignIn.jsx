e81d3cd94bd3118944dd37f01de1b7b09b21be3e
marshnil_13 <btech10225.20@bitmesra.ac.in>
2022-06-02 11:26:03 +0530

    user sign-in

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -2,6 +2,8 @@ import {useState} from 'react'
 import {Link, useNavigate} from 'react-router-dom'
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+import {getAuth, signInWithEmailAndPassword} from 'firebase/auth'
+import { async } from '@firebase/util'
 
 function SignIn() {
   const [showPassword, setShowPassword] = useState(false)
@@ -19,6 +21,24 @@ function SignIn() {
 const {email, password} = formData
 const navigate = useNavigate()
 
+const onSubmit = async (e) => {
+  e.preventDefault()
+  try{
+  const auth = getAuth()
+
+  const userCredential = await signInWithEmailAndPassword(
+    auth,
+    email,
+    password
+  )
+  if(userCredential.user){
+    navigate('/')
+  }
+  }catch(error){
+    console.log(error);
+  }
+}
+
   return (
     <>
         <div className="pageContainer">
@@ -27,7 +47,7 @@ const navigate = useNavigate()
           </header>
 
           <div className='form-control w-full max-w-xs'></div>
-          <form>
+          <form onSubmit={onSubmit}>
             <input 
             type='email'
             className='emailInput'
