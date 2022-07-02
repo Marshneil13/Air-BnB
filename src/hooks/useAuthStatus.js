65638723dc2205774054ff9d939c16fd3df06ffd
marshnil_13 <btech10225.20@bitmesra.ac.in>
2022-07-03 00:09:44 +0530

    forgot password setup

--- src/hooks/useAuthStatus.js
+++ src/hooks/useAuthStatus.js
@@ -5,6 +5,7 @@ import {getAuth, onAuthStateChanged} from 'firebase/auth'
 export const useAuthStatus = () => {
     const [loggedIn, setLoggedIn] = useState(false)
     const [checkingStatus, setCheckingStatus] = useState(true)
+    //Memory leak fixed
     const isMounted = useRef(true)//useRef returns an object
 
     useEffect(()=>{
