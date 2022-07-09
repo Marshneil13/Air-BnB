bd1e3e8ad664d6ab76f1b3fb70e3bd757e0fc8a9
marshnil_13 <btech10225.20@bitmesra.ac.in>
2022-07-09 13:44:02 +0530

    google authentication and explore page setup

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -3,6 +3,7 @@ import {Link, useNavigate} from 'react-router-dom'
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 import {getAuth, signInWithEmailAndPassword} from 'firebase/auth'
+import OAuth from '../components/OAuth'
 import { async } from '@firebase/util'
 import {toast} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
@@ -81,7 +82,7 @@ const onSubmit = async (e) => {
              </div>
           </form>
           
-          {/* Google OAuth */}
+          <OAuth/>
 
           <Link to='/sign-up' className='registerLink'>
             Sign Up Instead
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,6 +2,7 @@ import {useState} from 'react'
 import {Link, useNavigate} from 'react-router-dom'
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+import OAuth from '../components/OAuth'
 import {getAuth, createUserWithEmailAndPassword, updateProfile} from 'firebase/auth'
 import {setDoc, doc, serverTimestamp} from 'firebase/firestore'
 import {db} from '../firebase.config'
@@ -95,7 +96,7 @@ await setDoc(doc(db, 'users', user.uid), formDataCopy)
              </div>
           </form>
           
-          {/* Google OAuth */}
+          <OAuth/>
 
           <Link to='/sign-in' className='registerLink'>
             Sign In Instead
