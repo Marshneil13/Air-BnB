8d7efc021381fbd3aa0407e9c4fd8ac63bad12f4
marshnil_13 <btech10225.20@bitmesra.ac.in>
2022-07-12 12:03:33 +0530

    listing item 2

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -5,7 +5,7 @@ import bedIcon from '../assets/svg/bedIcon.svg'
 import bathtubIcon from '../assets/svg/bathtubIcon.svg'
 
 
-function ListingItem({listing, id}) {
+function ListingItem({listing, id, onDelete}) {
 
     console.log("LISTING", listing)
 
@@ -17,7 +17,37 @@ function ListingItem({listing, id}) {
             src={listing.imageUrls[0]} 
             alt={listing.name}
             className='categoryListingImg'/>
+            <div className="categoryListingDetails">
+                <p className="categoryListingLocation">
+                    {listing.location}
+                </p>
+                <p className="categoryListingName">
+                    {listing.name}
+                </p>
+                <p className="categoryListingPrice">
+                    ${listing.offer? listing.discountedPrice.toString().replace(
+                        /\B(?=(\d{3})+(?!\d))/g, ',')
+                    
+                    : listing.regularPrice.toString().replace(
+                        /\B(?=(\d{3})+(?!\d))/g, ',')}
+                        {listing.type === 'rent' && ' / Month'}
+                </p>
+                <div className="categoryListingInfoDiv">
+                    <img src={bedIcon} alt="bed" />
+                    <p className="categoryListingInfoText">
+                        {listing.bedrooms>1 ? `${listing.bedrooms} Bedrooms`: '1 Bedroom'}
+                    </p>
+                    <img src={bathtubIcon} alt="bath" />
+                    <p className="categoryListingInfoText">
+                        {listing.bathrooms>1 ? `${listing.bathrooms} Bathrooms`: '1 Bathroom'}
+                    </p>
+                </div>
+            </div>
         </Link>
+
+        {onDelete && (
+            <DeleteIcon className='removeIcon' fill='rgb(231, 76, 60)'/>
+        )}
     </li>
   )
 }
